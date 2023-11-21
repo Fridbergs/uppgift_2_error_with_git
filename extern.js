fc8949f8ea0441ca67890011f32edd1a18bffd61
Linus Fridberg <linus@fridbergs.com>
2023-11-21 20:22:12 +0100

    all external code works

--- extern.js
+++ extern.js
@@ -49,3 +49,60 @@ document.addEventListener("DOMContentLoaded", function () {
   // Load the API key and initialize Google Maps
   getApiKey();
 });
+
+// Function to get driving distance and time using the Google Maps Directions API
+async function getDrivingDistanceAndTime(
+  startLocation,
+  destination,
+  googleApiKey
+) {
+  try {
+    const response = await fetch(
+      `https://maps.googleapis.com/maps/api/directions/json?origin=${encodeURIComponent(
+        startLocation
+      )}&destination=${encodeURIComponent(
+        destination
+      )}&mode=driving&key=${googleApiKey}`
+    );
+
+    if (response.ok) {
+      const data = await response.json();
+
+      if (data.status === "OK") {
+        const route = data.routes[0];
+        const distance = route.legs[0].distance.text;
+        const duration = route.legs[0].duration.text;
+
+        return { distance, duration };
+      } else {
+        return { error: `Directions API error: ${data.status}` };
+      }
+    } else {
+      return { error: `HTTP error: ${response.status}` };
+    }
+  } catch (error) {
+    return { error: "Error fetching driving directions: " + error.message };
+  }
+}
+
+// Function to handle the button click event and display the result
+async function calculateDistanceAndTime() {
+  const startLocation = document.getElementById("startLocation").value;
+  const destination = "Your destination location"; // Replace with your destination location
+  const googleApiKey = "Your Google Maps API Key"; // Replace with your actual API key
+
+  const resultContainer = document.getElementById("result");
+
+  const { distance, duration, error } = await getDrivingDistanceAndTime(
+    startLocation,
+    destination,
+    googleApiKey
+  );
+
+  if (error) {
+    resultContainer.innerHTML = `<p>Error: ${error}</p>`;
+  } else {
+    resultContainer.innerHTML = `<p>Driving distance: ${distance}</p><p>Driving time: ${duration}</p>`;
+  }
+  getApiKey();
+}
