ca9bb55eb8b61047fa5f5ed1b2ac19364ee8ea45
Linus Fridberg <linus@fridbergs.com>
2023-11-21 18:44:59 +0100

    made some major changes

--- extern.js
+++ extern.js
@@ -1,60 +1,51 @@
-// Function to get API key from json
+// Function to get the API key located inside the JSON object
 async function getApiKey() {
-  const response = await fetch("variables.json");
-  if (response.ok) {
-    const contactInfo = await response.json();
-    //Get the json information
-    let googleApiKey = contactInfo["allApis"].googleApi;
-    initMap(googleApiKey);
-  } else {
-    console.log(`HTTP error message: ${response.status}`);
+  try {
+    const response = await fetch("variables.json");
+    if (response.ok) {
+      const variables = await response.json();
+      const googleApiKey = variables["allApis"].googleApi2;
+      initializeGoogleMaps(googleApiKey);
+    } else {
+      console.log(`HTTP error message: ${response.status}`);
+    }
+  } catch (error) {
+    console.error("Error fetching API key:", error);
   }
 }
 
-getApiKey();
+// Use the loaded JSON to initialize the Google Maps API
+function initializeGoogleMaps(googleApiKey) {
+  const script = document.createElement("script");
+  script.src = `https://maps.googleapis.com/maps/api/js?key=${googleApiKey}&callback=initializeMap`;
+  document.head.appendChild(script);
+}
 
-// Function to initialize the maps
-function initMap(apiKey) {
-  // Initialize map centered at your preset location
-  const presetLocation = { lat: 55.592529296875, lng: 12.97431755065918 };
-  const map = new google.maps.Map(document.getElementById("map2"), {
-    zoom: 15,
-    center: presetLocation,
+// Callback function to initialize the map after the Google Maps API has loaded
+function initializeMap() {
+  // Set the default coordinates (you can adjust these to your desired location)
+  const defaultLat = 55.59257971038269;
+  const defaultLng = 12.974460810106853;
+
+  // Create a LatLng object representing the default location
+  const myLatLng = new google.maps.LatLng(defaultLat, defaultLng);
+
+  // Create a map object and specify the DOM element for display
+  const map = new google.maps.Map(document.getElementById("map"), {
+    center: myLatLng,
+    zoom: 14, // Adjust the zoom level as needed
   });
 
-  // Add a marker for the preset location
-  new google.maps.Marker({
-    position: presetLocation,
+  // Create a marker and set its position
+  const marker = new google.maps.Marker({
     map: map,
-    title: "Preset Location",
-  });
-  console.log(apiKey);
-}
-
-// Function to calculate distance
-function calculateDistance() {
-  const userLocationInput = document.getElementById("userLocation").value;
-
-  // Use Google Maps Geocoding API to convert user's location input to coordinates
-  const geocoder = new google.maps.Geocoder();
-  geocoder.geocode({ address: userLocationInput }, function (results, status) {
-    if (status === "OK" && results[0]) {
-      const userLocation = results[0].geometry.location;
-
-      // Use Google Maps Geometry Library to calculate distance
-      const presetLocation = new google.maps.LatLng(37.7749, -122.4194);
-      const distance = google.maps.geometry.spherical.computeDistanceBetween(
-        userLocation,
-        presetLocation
-      );
-
-      alert(
-        `Distance from your location to preset location: ${distance.toFixed(
-          2
-        )} meters`
-      );
-    } else {
-      alert("Geocoding failed. Please enter a valid location.");
-    }
+    position: myLatLng,
+    title: "My Location", // Set the title for the marker (tooltip)
   });
 }
+
+// Add an event listener for when the DOM has fully loaded
+document.addEventListener("DOMContentLoaded", function () {
+  // Load the API key and initialize Google Maps
+  getApiKey();
+});
